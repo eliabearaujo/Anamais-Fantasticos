a9dfaaa9d2bcbc880c3e7f69db20bdac0e5c7830
eliabearaujo <eliabe_trajano@hotmail.com>
2022-10-20 18:18:27 -0300

    Refatorando o modal

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,7 +1,9 @@
-export default function initModal() {
-  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-  const botaoFechar = document.querySelector('[data-modal="fechar"]');
-  const containerModal = document.querySelector('[data-modal="container"]');
+export default class initModal {
+  constructor(abrir,fechar,container){
+    this.botaoAbrir = document.querySelector(abrir);
+    this.botaoFechar = document.querySelector(fechar);
+    this.containerModal = document.querySelector(container);
+  }
 
   function toggleModal(event) {
     event.preventDefault();
